92f0dd1dec51ea6de1fcdeb6c1783e88a615453c
Mukul23022005 <152482515+Mukul23022005@users.noreply.github.com>
2023-11-30 14:35:12 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,248 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Veg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 230 ,
+    img: 'https://vegecravings.com/wp-content/uploads/2016/07/veg-biryani-recipe-step-by-step-instructions.jpg.webp',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 170,
+    img: 'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 3,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.7,
+    price: 450,
+    img: 'https://static.toiimg.com/thumb/52568357.cms?imgsize=158081&width=800&height=800',
+    quantity: 1
+},
+
+{
+    id: 4,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 250,
+    img: 'https://www.vegrecipesofindia.com/wp-content/uploads/2013/06/kashmiri-biryani-recipe-11.jpg',
+    quantity: 1
+},
+
+{
+    id: 5,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.5,
+    price: 280,
+    img: 'https://b.zmtcdn.com/data/pictures/4/19108944/021130601cd2b36354732967177173af_o2_featured_v2.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 350,
+    img: 'https://static.toiimg.com/thumb/53007558.cms?imgsize=518651&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Chicken masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 250,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2022/08/chicken-masala-authentic.jpg',
+    quantity: 1
+},
+
+{
+    id: 7,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 300,
+    img: 'https://www.kitchensanctuary.com/wp-content/uploads/2022/09/Air-Fryer-Chicken-Curry-square-FS-36.jpg',
+    quantity: 1
+},
+
+{
+    id: 10,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 170,
+    img: 'https://www.indianrecipeinfo.com/wp-content/uploads/2021/12/Chicken-Handi-Recipe-480x270.jpg',
+    quantity: 1
+},
+
+{
+    id: 11,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 150,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/03/matar-paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 12,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'https://foreignfork.com/wp-content/uploads/2023/07/EasyPalakPaneer-FEATURE.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Paneer bhurgi',
+    category : 'paneer',
+    rating : 4.3,
+    price: 145,
+    img: 'https://www.sharmispassions.com/wp-content/uploads/2021/09/PaneerBhurji1-500x500.jpg',
+    quantity: 1
+
+},
+
+{
+    id: 16,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 175,
+    img: 'https://static.toiimg.com/thumb/52446409.cms?width=1200&height=900',
+    quantity: 1
+},
+
+
+{
+    id: 18,
+    name: 'Veg dum Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 120,
+    img: 'https://www.bigbasket.com/media/uploads/recipe/w-l/1030_1.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 170,
+    img: 'https://greenbowl2soul.com/wp-content/uploads/2021/06/Indian-vegetable-curry.jpg',
+    quantity: 1
+},
+
+{
+    id: 20,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 128,
+    img: 'https://www.veggiesmadeeasy.com/wp-content/uploads/2017/07/Vegetable-Masala-2.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 60,
+    img: 'https://www.scrumptiously.com/wp-content/uploads/2023/02/VegetablePakora.webp',
+    quantity: 1
+},
+
+{
+    id: 24,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 160,
+    img: 'https://daruadda.com/wp-content/uploads/2023/01/Veg-Chowmin.png',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 140,
+    img: 'https://www.cubesnjuliennes.com/wp-content/uploads/2021/01/Spring-Roll-Recipe.jpg',
+    quantity: 1
+},
+
+{
+    id: 26,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 80,
+    img: 'https://www.nestleprofessional.in/sites/default/files/2022-07/Fried-Rice.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 180,
+    img: 'https://www.cookwithkushi.com/wp-content/uploads/2021/12/IMG_5820g.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 120,
+    img: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/idli-recipe-4.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 30,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 115,
+    img: 'https://static.toiimg.com/thumb/53537841.cms?imgsize=368508&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 140,
+    img: 'https://static.toiimg.com/thumb/53239433.cms?imgsize=247810&width=800&height=800',
+    quantity: 1
+},
+
+{
+    id: 33,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 120,
+    img: 'https://vegecravings.com/wp-content/uploads/2018/02/Medu-Vada-Recipe-Step-By-Step-Instructions.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
